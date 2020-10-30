0726b32abd516eca3f533adc5fc40d3e4c5c46c9
mehtadeep007 <mehtadeep2013@gmail.com>
2020-10-30 12:08:07 +0530

    Context api management

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import React, { useEffect,useState } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
 import './App.css';
-import Login from "./Login/Login"
+import Login from "./Login/Login";
+import Player from "./Player/Player"
 import { getTokenFromResponse } from "./spotify/spotify";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-
-  const[token, setToken]=useState(null);
+  const [{user,token}, dispatch] = useStateValue();
 
   useEffect(()=>{
+    //getting the token from url
     const hash = getTokenFromResponse();
-    window.location.hash = "";
+    window.location.hash = "";  //making url empty
 
     let _token=hash.access_token;
     if(_token){
-      setToken(_token)
 
-      spotify.setAccessToken(_token)
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
+      spotify.setAccessToken(_token); //setting up personal token
+
+      //updating user state using contextapi
       spotify.getMe()
       .then(user=>{
-        console.log("user: ",user)
-      })
+        dispatch({
+          type: "SET_USER",
+          user:user,
+        });
+      });
     }
-    console.log(_token)
+
   },[]);
 
 
   return (
     <div className="App">
       {
-        token?(<Player/>):(<Login/>)
+        token?(<Player spotify={spotify}/>):(<Login/>)
       }
     </div>
   );
